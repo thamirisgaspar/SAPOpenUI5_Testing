526302e3381f08d21a90d1b74e88231357ac8358
T. Gaspar Da Silva <t.gaspar.da.silva@avanade.com>
2020-03-25 11:35:13 -0300

    Testing a New Module

---
+++ webapp/model/FlaggedType.js
@@ -0,0 +1,13 @@
+sap.ui.define([
+	"sap/ui/model/SimpleType"
+], function (SimpleType) {
+	"use strict";
+	return SimpleType.extend("sap.ui.demo.bulletinboard.model.FlaggedType", {
+		formatValue: function () {
+		},
+		parseValue: function () {
+		},
+		validateValue: function () {
+		}
+	});
+});
